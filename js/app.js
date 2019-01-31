39212163c2077fba38af1b22a051338b32f3d8bc
A.J. Bale <abalejr@gmail.com>
2019-01-31 10:50:35 -0600

    Create localStorage items for each post at init

--- js/app.js
+++ js/app.js
@@ -1,23 +1,24 @@
 
 /**
- * Main app file.  Initializes app components.
+ * Main app file. Initializes app components.
  */
-
-
+const pageContentEl = document.getElementById('pageContent');
 /**
  * The main app object.
  *
  */
 var vanillaPress = {
 
-  init: function() {
+    init: function() {
 
-    // Add any functions here you want
-    // to run to start the application
-    console.log( jsonData );
-
-  }
-
+        // Add any functions here you want
+        // to run to start the application
+        console.log( jsonData );
+        for (i = 0; i < JSON.parse(jsonData).length; i++) {
+            let postData = JSON.parse(jsonData)[i];
+            localStorage.setItem(postData['slug'], JSON.stringify(postData));
+        }
+    },
 };
 
 vanillaPress.init();
