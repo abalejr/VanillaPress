13b1523eb39aa1b8f715d40976e548ed324000f7
A.J. Bale <abalejr@gmail.com>
2019-01-31 11:16:58 -0600

    Create vanillaPress.displayAll()

--- js/app.js
+++ js/app.js
@@ -19,8 +19,30 @@ var vanillaPress = {
             localStorage.setItem(postData['slug'], JSON.stringify(postData));
         }
     },
+    displayAll: function() {
+        for (i = 0; i < JSON.parse(jsonData).length; i++) {
+            var currentData = JSON.parse(jsonData)[i],
+                currentSlug = currentData['slug'],
+                currentPost = JSON.parse(localStorage.getItem(currentSlug)),
+                postTitle = document.createTextNode(currentPost.title),
+                postContent = document.createTextNode(currentPost.content),
+                postHeaderEl = document.createElement('h3'),
+                postLinkEl = document.createElement('a'),
+                postEl = document.createElement('article');
+
+            postLinkEl.appendChild(postTitle);
+            postHeaderEl.appendChild(postLinkEl);
+            postEl.appendChild(postHeaderEl);
+            postEl.appendChild(postContent);
+            pageContentEl.appendChild(postEl);
+
+            postLinkEl.setAttribute('href', '#' + currentSlug);
+        }
+    }
 };
 
 vanillaPress.init();
 
 // Add your custom code starting here:
+
+vanillaPress.displayAll();
