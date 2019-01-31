b11d112d4a04670f2489039483cc58e9fee4e3ad
A.J. Bale <abalejr@gmail.com>
2019-01-31 12:30:20 -0600

    Add readability spaces to function calls

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 /**
  * Main app file. Initializes app components.
  */
-const pageContentEl = document.getElementById('pageContent');
+const pageContentEl = document.getElementById( 'pageContent' );
 /**
  * The main app object.
  *
@@ -14,27 +14,27 @@ const vanillaPress = {
         // Add any functions here you want
         // to run to start the application
         console.log( jsonData );
-        for (i = 0; i < JSON.parse(jsonData).length; i++) {
-            let postData = JSON.parse(jsonData)[i];
-            localStorage.setItem(postData['slug'], JSON.stringify(postData));
+        for (i = 0; i < JSON.parse( jsonData ).length; i++) {
+            let postData = JSON.parse( jsonData )[i];
+            localStorage.setItem( postData['slug'], JSON.stringify( postData ) );
         }
     },
     displayAll: function() {
         for (i = 0; i < localStorage.length; i++) {
-            let currentPost = JSON.parse(localStorage.getItem(localStorage.key(i))),
+            let currentPost = JSON.parse( localStorage.getItem( localStorage.key( i ) ) ),
                 currentSlug = currentPost.slug,
-                postTitle = document.createTextNode(currentPost.title),
+                postTitle = document.createTextNode( currentPost.title ),
                 postContent = currentPost.content,
-                postHeaderEl = document.createElement('h3'),
-                postLinkEl = document.createElement('a'),
-                postEl = document.createElement('article');
+                postHeaderEl = document.createElement( 'h3' ),
+                postLinkEl = document.createElement( 'a' ),
+                postEl = document.createElement( 'article' );
 
             postLinkEl.setAttribute('href', '#' + currentSlug);
 
-            postLinkEl.appendChild(postTitle);
-            postHeaderEl.appendChild(postLinkEl);
-            postEl.appendChild(postHeaderEl);
-            pageContentEl.appendChild(postEl);
+            postLinkEl.appendChild( postTitle );
+            postHeaderEl.appendChild( postLinkEl );
+            postEl.appendChild( postHeaderEl );
+            pageContentEl.appendChild( postEl );
             postEl.innerHTML += postContent;
         }
     }
